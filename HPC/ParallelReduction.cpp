fc7200964a0684349625c86770e6ac9c32b356eb
AJ <74534547+aj-in@users.noreply.github.com>
2023-05-30 04:50:33 +0530

    Add files via upload

---
+++ HPC/ParallelReduction.cpp
@@ -0,0 +1,83 @@
+/*
+  Windows does not support user defined reductions.
+  This program may not run on MVSC++ compilers for Windows.
+  Please use Linux Environment.[You can try using "windows subsystem for linux"]
+*/
+
+#include<iostream>
+#include<omp.h>
+
+using namespace std;
+int minval(int arr[], int n){
+  int minval = arr[0];
+  #pragma omp parallel for reduction(min : minval)
+    for(int i = 0; i < n; i++){
+      if(arr[i] < minval) minval = arr[i];
+    }
+  return minval;
+}
+
+int maxval(int arr[], int n){
+  int maxval = arr[0];
+  #pragma omp parallel for reduction(max : maxval)
+    for(int i = 0; i < n; i++){
+      if(arr[i] > maxval) maxval = arr[i];
+    }
+  return maxval;
+}
+
+int sum(int arr[], int n){
+  int sum = 0;
+  #pragma omp parallel for reduction(+ : sum)
+    for(int i = 0; i < n; i++){
+      sum += arr[i];
+    }
+  return sum;
+}
+
+int average(int arr[], int n){
+  return (double)sum(arr, n) / n;
+}
+
+int main(){
+  int n = 5;
+  int arr[] = {1,2,3,4,5};
+  cout << "The minimum value is: " << minval(arr, n) << '\n';
+  cout << "The maximum value is: " << maxval(arr, n) << '\n';
+  cout << "The summation is: " << sum(arr, n) << '\n';
+  cout << "The average is: " << average(arr, n) << '\n';
+  return 0;
+}
+
+
+/*The function minval takes an array arr and its size n as input.
+It initializes a variable minval with the first element of the array.
+The #pragma omp parallel for reduction(min : minval) directive indicates that the following loop will be executed in parallel. Each thread will find the minimum value from its portion of the array, and then the minimum values found by each thread will be combined using the reduction operation min to obtain the overall minimum value.
+Inside the loop, each thread compares the array element at index i with the current minval and updates minval if a smaller value is found.
+Finally, the function returns the minimum value.
+The explanations for the remaining functions (maxval, sum, and average) are similar. They use the reduction clause with different operators (max and +) to perform parallel reductions for finding the maximum value, sum, and average of the array, respectively.
+
+The sum function takes an array arr and its size n as input.
+It initializes an integer variable sum to store the sum of the array elements.
+The #pragma omp parallel for reduction(+ : sum) directive indicates that the following loop will be executed in parallel. Each thread will compute the sum of its portion of the array, and then the partial sums computed by each thread will be combined using the reduction operation + to obtain the overall sum.
+Inside the loop, each thread adds the value of arr[i] to its local sum variable.
+Finally, the function returns the overall sum.
+By using OpenMP's reduction clause with + operator, the work of summing the array elements is divided among multiple threads, allowing for parallel execution. The reduction operation ensures that the partial sums from each thread are correctly combined to produce the final sum.
+
+
+
+
+Certainly! Here's an explanation of the average function in simple points:
+
+The average function takes an array arr and its size n as input.
+It calculates the sum of the array elements by calling the sum function, passing the array and its size as arguments.
+The sum is divided by n to compute the average.
+To ensure that the division is performed with floating-point precision, the result is cast to double using (double) before dividing.
+The resulting average value is returned.
+In summary, the average function calculates the average value of the elements in the array by dividing the sum of the elements by the number of elements.
+In the main function:
+
+An array arr is initialized with values.
+The minimum value, maximum value, sum, and average of the array are computed by calling the respective functions.
+The results are printed using cout.*/
+
